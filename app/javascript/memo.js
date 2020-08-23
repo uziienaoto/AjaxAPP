1984c4da5090b5baf2c4abe423774f3d8dcc90a0
uziienaoto <otoaneiizu@gmail.com>
2020-08-23 18:54:07 +0900

    Update memo.js

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,6 +1,7 @@
 function memo() {
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
+    console.log("test")
     const formData = new FormData(document.getElementById("form"));
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
